a06070d632c3b6784e4d1078aa4f0bddfcaf0161
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-03-30 19:01:02 +0000

    navigation updates

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,7 +2,7 @@ import React, {useState} from 'react';
 import { Link } from 'react-router-dom';
 
 import { MenuIcon, XIcon } from '@heroicons/react/outline';
-import { FaPlay } from 'react-icons/fa'
+import { FaHeadphones } from 'react-icons/fa'
 
 export default function Navigation() {
     const [nav, setNav] = useState(false)
@@ -12,10 +12,10 @@ export default function Navigation() {
     const handleClose =()=> setNav(!nav)
     return (
 
-        <div className='w-screen h-[70px] z-10 bg-green-600 fixed drop-shadow-lg'>
+        <div className='w-screen h-[70px] z-10 bg-purple-500 fixed drop-shadow-lg'>
       <div className='px-2 flex justify-between items-center w-full h-full'>
         <div className='flex items-center'>
-        <FaPlay className=' text-black lg:w-[35px] lg:h-[35px]' />
+        <FaHeadphones className=' text-black lg:w-[35px] lg:h-[35px]' />
           <h1 className='text-3xl font-bold text-white mr-4 sm:text-4xl'>PLAYME.</h1>
           <ul className='hidden text-white md:flex'>
           <li><Link to="/">Home</Link></li>
