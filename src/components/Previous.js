d11c0cc7caa5f7e4bb01b1fa311656ee1abbba94
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-05-05 23:03:47 +0000

    previous updates

--- src/components/Previous.js
+++ src/components/Previous.js
@@ -1,8 +1,156 @@
 import React from 'react'
+import Navigation from './Navigation'
 
 function Previous() {
   return (
     <div>
+        <Navigation />
+
+
+         {/* continents */}
+         <div className="flex flex-col bg-white m-auto pt-10">
+        <h1 className="flex py-5 lg:mx-20 md:mx-10 mx-5 font-bold text-3xl text-gray-800">
+                Continents
+        </h1>
+        <div className="flex overflow-x-scroll pb-10 hide-scroll-bar">
+            <div className="flex flex-nowrap lg:ml-200 md:ml-20 ml-10 ">
+
+            <div className="inline-block px-3">
+                <div className="w-64 h-max max-w-xs overflow-hidden rounded-lg shadow-md bg-green-600 hover:shadow-xl transition-shadow duration-300 ease-in-out">
+
+                    <div className="relative p-4 sm:p-6 lg:p-8">
+
+                        <p className="text-sm font-medium capitalize tracking-widest text-white">
+                            Actions in the Black Community
+                        </p>
+
+                        <p className="text-xl font-bold text-white sm:text-2xl">
+                            Africa
+                        </p>
+
+                    </div>
+
+                </div>
+            </div>
+
+            <div className="inline-block px-3">
+                <div className="w-64 h-max max-w-xs overflow-hidden rounded-lg shadow-md bg-blue-600 hover:shadow-xl transition-shadow duration-300 ease-in-out">
+
+                    <div className="relative p-4 sm:p-6 lg:p-8">
+
+                        <p className="text-sm font-medium capitalize tracking-widest text-white">
+                            Actions In The Second-Smallest Continent
+                        </p>
+
+                        <p className="text-xl font-bold text-white sm:text-2xl">
+                            Europe
+                        </p>
+
+                    </div>
+
+                </div>
+            </div>
+
+           
+            <div className="inline-block px-3">
+                <div className="w-64 h-max max-w-xs overflow-hidden rounded-lg shadow-md bg-orange-400 hover:shadow-xl transition-shadow duration-300 ease-in-out">
+
+                    <div className="relative p-4 sm:p-6 lg:p-8">
+
+                        <p className="text-sm font-medium capitalize tracking-widest text-white">
+                            Actions In The Largest Continent
+                        </p>
+
+                        <p className="text-xl font-bold text-white sm:text-2xl">
+                            Asia
+                        </p>
+
+                    </div>
+
+                </div>
+            </div>
+
+
+            <div className="inline-block px-3">
+                <div className="w-64 h-max max-w-xs overflow-hidden rounded-lg shadow-md bg-pink-600 hover:shadow-xl transition-shadow duration-300 ease-in-out">
+
+                    <div className="relative p-4 sm:p-6 lg:p-8">
+
+                        <p className="text-sm font-medium capitalize tracking-widest text-white">
+                            Actions In The Fouth-Largest Continent
+                        </p>
+
+                        <p className="text-xl font-bold text-white sm:text-2xl">
+                            South America
+                        </p>
+
+                    </div>
+
+                </div>
+            </div>
+
+
+            <div className="inline-block px-3">
+                <div className="w-64 h-max max-w-xs overflow-hidden rounded-lg shadow-md bg-yellow-400 hover:shadow-xl transition-shadow duration-300 ease-in-out">
+
+                    <div className="relative p-4 sm:p-6 lg:p-8">
+
+                        <p className="text-sm font-medium capitalize tracking-widest text-white">
+                            Actions In The Third-Largest Continent
+                        </p>
+
+                        <p className="text-xl font-bold text-white sm:text-2xl">
+                            North America
+                        </p>
+
+                    </div>
+
+                </div>
+            </div>
+
+
+            <div className="inline-block px-3">
+                <div className="w-64 h-max max-w-xs overflow-hidden rounded-lg shadow-md bg-red-700 hover:shadow-xl transition-shadow duration-300 ease-in-out">
+
+                    <div className="relative p-4 sm:p-6 lg:p-8">
+
+                        <p className="text-sm font-medium capitalize tracking-widest text-white">
+                            Climate Action Targeting Renewables
+                        </p>
+
+                        <p className="text-xl font-bold text-white sm:text-2xl">
+                            Renewables
+                        </p>
+
+                    </div>
+
+                </div>
+            </div>
+
+            <div className="inline-block px-3">
+                <div className="w-64 h-max max-w-xs overflow-hidden rounded-lg shadow-md bg-gray-400 hover:shadow-xl transition-shadow duration-300 ease-in-out">
+
+                    <div className="relative p-4 sm:p-6 lg:p-8">
+
+                        <p className="text-sm font-medium capitalize tracking-widest text-white">
+                            Any Category Not Mentioned
+                        </p>
+
+                        <p className="text-xl font-bold text-white sm:text-2xl">
+                            Others
+                        </p>
+
+                    </div>
+
+                </div>
+            </div> 
+            
+           
+           
+            
+            </div>
+        </div>
+    </div>
         
     </div>
   )
