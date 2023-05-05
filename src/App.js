2b8bb7dd4059fb0cddde2b69a9575bd0a3a3d2fd
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-05-05 23:07:02 +0000

    app updates

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./components/HomePage";
+import Previous from "./components/Previous";
 
 function App() {
   return (
     <Router>
       <Routes>
       <Route path="/" exact element={<HomePage />} />
+      <Route path="/previous" exact element={<Previous />} />
       </Routes>
     </Router>
   );
