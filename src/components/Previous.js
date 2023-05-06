9035f4dcdf0292d89f85135f351c0493245b227c
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-05-06 13:06:57 +0000

    previous updates

--- src/components/Previous.js
+++ src/components/Previous.js
@@ -373,11 +373,6 @@ function Previous() {
            
         </div>
 
-        <div className="relative ">
-        <div className="absolute inset-0 flex items-center">
-            <div className="w-full border-b border-gray-300"></div>
-        </div>
-        </div>
 
     </div>
         
