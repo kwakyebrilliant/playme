d7499699a1dc2a6ea557c7f8756163e87e3808e0
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-05-06 13:30:03 +0000

    earners table

--- src/components/Earners.js
+++ src/components/Earners.js
@@ -154,46 +154,40 @@ function Earners() {
             <thead className="ltr:text-left rtl:text-right">
             <tr>
                 <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
-                Name
+                Number
                 </th>
                 <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
-                Date of Birth
+                Address
                 </th>
                 <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
-                Role
-                </th>
-                <th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
-                Salary
+                Amount
                 </th>
             </tr>
             </thead>
 
             <tbody className="divide-y divide-gray-200">
             <tr>
-                <td className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
-                John Doe
+                <td className="whitespace-nowrap px-4 py-2 font-medium text-center text-gray-900">
+                #1
                 </td>
-                <td className="whitespace-nowrap px-4 py-2 text-gray-700">24/05/1995</td>
-                <td className="whitespace-nowrap px-4 py-2 text-gray-700">Web Developer</td>
-                <td className="whitespace-nowrap px-4 py-2 text-gray-700">$120,000</td>
+                <td className="whitespace-nowrap px-4 py-2 text-center text-gray-700">24/05/1995</td>
+                <td className="whitespace-nowrap px-4 py-2 text-center text-gray-700">Web Developer</td>
             </tr>
 
             <tr>
-                <td className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
-                Jane Doe
+                <td className="whitespace-nowrap px-4 py-2 text-center font-medium text-gray-900">
+                #2
                 </td>
-                <td className="whitespace-nowrap px-4 py-2 text-gray-700">04/11/1980</td>
-                <td className="whitespace-nowrap px-4 py-2 text-gray-700">Web Designer</td>
-                <td className="whitespace-nowrap px-4 py-2 text-gray-700">$100,000</td>
+                <td className="whitespace-nowrap px-4 py-2 text-center text-gray-700">04/11/1980</td>
+                <td className="whitespace-nowrap px-4 py-2 text-center text-gray-700">Web Designer</td>
             </tr>
 
             <tr>
-                <td className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
-                Gary Barlow
+                <td className="whitespace-nowrap px-4 py-2 text-center font-medium text-gray-900">
+                #3
                 </td>
-                <td className="whitespace-nowrap px-4 py-2 text-gray-700">24/05/1995</td>
-                <td className="whitespace-nowrap px-4 py-2 text-gray-700">Singer</td>
-                <td className="whitespace-nowrap px-4 py-2 text-gray-700">$20,000</td>
+                <td className="whitespace-nowrap px-4 py-2 text-center text-gray-700">24/05/1995</td>
+                <td className="whitespace-nowrap px-4 py-2 text-center text-gray-700">Singer</td>
             </tr>
             </tbody>
         </table>
