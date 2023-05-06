66bb1d7a9c511f02587e02d75b2efb8e11b86b98
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-05-06 12:20:20 +0000

    earners updates

--- src/components/Earners.js
+++ src/components/Earners.js
@@ -7,6 +7,20 @@ function Earners() {
         <Navigation />
 
 
+        <div className="flex flex-col bg-white m-auto px-20 pt-36">
+
+            <div>
+                <label className="sr-only" for="user">User</label>
+                <input
+                className="w-full rounded-lg shadow-md p-3 text-sm"
+                placeholder="Search user"
+                type="text"
+                id="user"
+                />
+            </div>
+        </div>
+
+
        <h1>Earners</h1> 
     </div>
   )
