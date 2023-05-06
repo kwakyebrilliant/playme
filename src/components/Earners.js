b593ef26659f79d6df3223da2ab8b0ac5f959dcf
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-05-06 12:53:18 +0000

    earners updates

--- src/components/Earners.js
+++ src/components/Earners.js
@@ -30,7 +30,7 @@ function Earners() {
         </h1>
 
         <div className='relative px-20 grid grid-cols-1 lg:grid-cols-3 gap-x-8 gap-y-16'>
-            <a className="relative flex items-start justify-between rounded-xl border border-gray-100 p-4 shadow-xl sm:p-6 lg:p-8" href="#">
+            <a className="relative flex items-start justify-between rounded-xl border border-gray-100 p-4 shadow-md sm:p-6 lg:p-8" href="#">
             <div className="pt-4 text-gray-500">
            
             <svg
@@ -53,15 +53,82 @@ function Earners() {
                 </h3>
 
                 <p className="mt-2 hidden text-sm sm:block">
-                You can manage phone, email and chat conversations all from a single
-                mailbox.
+                0x22B...6ba9
                 </p>
             </div>
 
             <span
                 className="rounded-full bg-green-100 px-3 py-1.5 text-xs font-medium text-green-600"
             >
-                4.3
+                #1
+            </span>
+            </a>
+
+            <a className="relative flex items-start justify-between rounded-xl border border-gray-100 p-4 shadow-md sm:p-6 lg:p-8" href="#">
+            <div className="pt-4 text-gray-500">
+           
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                stroke-width="2"
+            >
+                <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M17 9V7a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2m2 4h10a2 2 0 002-2v-6a2 2 0 00-2-2H9a2 2 0 00-2 2v6a2 2 0 002 2zm7-5a2 2 0 11-4 0 2 2 0 014 0z"
+                />
+            </svg>
+
+                <h3 className="mt-4 text-lg font-bold text-gray-900 sm:text-xl">
+                0.0007ETH
+                </h3>
+
+                <p className="mt-2 hidden text-sm sm:block">
+                0x22B...6ba9
+                </p>
+            </div>
+
+            <span
+                className="rounded-full bg-green-100 px-3 py-1.5 text-xs font-medium text-green-600"
+            >
+                #2
+            </span>
+            </a>
+
+            <a className="relative flex items-start justify-between rounded-xl border border-gray-100 p-4 shadow-md sm:p-6 lg:p-8" href="#">
+            <div className="pt-4 text-gray-500">
+           
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                stroke-width="2"
+            >
+                <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M17 9V7a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2m2 4h10a2 2 0 002-2v-6a2 2 0 00-2-2H9a2 2 0 00-2 2v6a2 2 0 002 2zm7-5a2 2 0 11-4 0 2 2 0 014 0z"
+                />
+            </svg>
+
+                <h3 className="mt-4 text-lg font-bold text-gray-900 sm:text-xl">
+                0.0007ETH
+                </h3>
+
+                <p className="mt-2 hidden text-sm sm:block">
+                0x22B...6ba9
+                </p>
+            </div>
+
+            <span
+                className="rounded-full bg-green-100 px-3 py-1.5 text-xs font-medium text-green-600"
+            >
+                #3
             </span>
             </a>
 
