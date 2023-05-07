88b8f230dbc829b121895da6a302215c9d2f6f48
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-05-07 13:13:25 +0000

    homne updates

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -103,19 +103,19 @@ const HomePage = () => {
             <img
               alt="Guitar"
               src="https://images.unsplash.com/photo-1609557927087-f9cf8e88de18?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80"
-              class="aspect-square h-full w-full object-cover"
+              className="aspect-square h-full w-full object-cover"
             />
           </div>
 
-          <div class="flex flex-1 flex-col justify-between">
-            <div class="border-s border-gray-900/10 p-4 sm:border-l-transparent sm:p-6">
+          <div className="flex flex-1 flex-col justify-between">
+            <div className="border-s border-gray-900/10 p-4 sm:border-l-transparent sm:p-6">
               <a href="#">
-                <h3 class="font-bold uppercase text-gray-900">
+                <h3 className="font-bold uppercase text-gray-900">
                   Finding the right guitar for your style - 5 tips
                 </h3>
               </a>
 
-              <p class="mt-2 line-clamp-3 text-sm/relaxed text-gray-700">
+              <p className="mt-2 line-clamp-3 text-sm/relaxed text-gray-700">
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae
                 dolores, possimus pariatur animi temporibus nesciunt praesentium dolore
                 sed nulla ipsum eveniet corporis quidem, mollitia itaque minus soluta,
@@ -124,10 +124,10 @@ const HomePage = () => {
               </p>
             </div>
 
-            <div class="sm:flex sm:items-end sm:justify-end">
+            <div className="sm:flex sm:items-end sm:justify-end">
               <a
                 href="#"
-                class="block bg-yellow-300 px-5 py-3 text-center text-xs font-bold uppercase text-gray-900 transition hover:bg-yellow-400"
+                className="block bg-yellow-300 px-5 py-3 text-center text-xs font-bold uppercase text-gray-900 transition hover:bg-yellow-400"
               >
                 Read Blog
               </a>
