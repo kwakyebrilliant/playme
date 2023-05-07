e6ec46ed87abf99ef4a2f8c65a683f722c014abe
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-05-07 22:24:14 +0000

    home updates

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -121,11 +121,9 @@ const HomePage = () => {
               </a>
 
               <p className="mt-2 line-clamp-3 text-sm/relaxed text-gray-700">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae
-                dolores, possimus pariatur animi temporibus nesciunt praesentium dolore
-                sed nulla ipsum eveniet corporis quidem, mollitia itaque minus soluta,
-                voluptates neque explicabo tempora nisi culpa eius atque dignissimos.
-                Molestias explicabo corporis voluptatem?
+                A user will have the opportunity to generate an english dictionary word
+                where he or she will specify the reward that will be given to anyone that
+                correctly spells the word.
               </p>
             </div>
 
