2d98f4b47a7fee8951bdc5ce7d155fcb8d548fc8
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-05-07 22:46:50 +0000

    app updates

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./components/HomePage";
 import Previous from "./components/Previous";
 import Earners from "./components/Earners";
+import Summary from "./dashboard/Summary";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Route path="/" exact element={<HomePage />} />
       <Route path="/previous" exact element={<Previous />} />
       <Route path="/earners" exact element={<Earners />} />
+      <Route path="/summary" exact element={<Summary />} />
       </Routes>
     </Router>
   );
