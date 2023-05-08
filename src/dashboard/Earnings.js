ba4e071bacacaac7725dd0d056aaa5bf06bc81e9
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-05-08 23:37:59 +0000

    earnings updates

--- src/dashboard/Earnings.js
+++ src/dashboard/Earnings.js
@@ -7,7 +7,21 @@ function Earnings() {
 
       <DashboardNav />
       
-      Earnings
+     <div className='lg:p-24 p-4'>
+
+      <div className='bg-white border flex justify-between lg:p-4 p-2'>
+        <div>
+        <p>Earnings</p>
+        <p>Earnings</p>
+        </div>
+        <div>
+        <p className='text-lg'>+ 0.02556988 ETH</p>
+        <p className='text-blue-400 text-sm'>+ $47.41 USD</p>
+        </div>
+        </div>
+        
+
+     </div>
 
     </div>
   )
