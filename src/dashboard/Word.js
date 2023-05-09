d10d0090813ca23116417caf79c7f94da6a50410
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-05-09 17:07:01 +0000

    word updates

--- src/dashboard/Word.js
+++ src/dashboard/Word.js
@@ -4,6 +4,8 @@ import DashboardNav from './DashboardNav'
 
 import trophy from '../assets/trophy.jpg';
 
+import { FaRegFileWord } from 'react-icons/fa'
+
 
 function Word() {
   return (
@@ -37,13 +39,6 @@ function Word() {
         <div className='grid md:grid-cols-2 gap-x-10 lg:p-4 p-2'>
         
         <div>
-        {/* <a className="block">
-        <img
-          alt="Signage"
-          src={trophy}
-          className="h-56 w-full rounded-bl-3xl rounded-tr-3xl object-cover sm:h-64 lg:h-52"
-        />
-        </a> */}
         <a style={{ backgroundImage: `url(${trophy})` }} className="relative block overflow-hidden rounded-xl bg-cover bg-center bg-no-repeat">
           <div className="absolute inset-0 bg-black/25"></div>
 
@@ -60,35 +55,22 @@ function Word() {
         <div>
             <a className="relative flex items-start justify-between rounded-xl bg-white border border-gray-100 p-4 shadow-xl sm:p-6 lg:p-8">
       <div className="pt-4 text-gray-500">
-        <svg
-          className="h-8 w-8 sm:h-10 sm:w-10"
-          fill="none"
-          stroke="currentColor"
-          viewBox="0 0 24 24"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M19.428 15.428a2 2 0 00-1.022-.547l-2.387-.477a6 6 0 00-3.86.517l-.318.158a6 6 0 01-3.86.517L6.05 15.21a2 2 0 00-1.806.547M8 4h8l-1 1v5.172a2 2 0 00.586 1.414l5 5c1.26 1.26.367 3.414-1.415 3.414H4.828c-1.782 0-2.674-2.154-1.414-3.414l5-5A2 2 0 009 10.172V5L8 4z"
-          ></path>
-        </svg>
+      <FaRegFileWord className=' text-gray-500 lg:w-8 lg:h-8 sm:h-10 sm:w-10'/>
 
         <h3 className="mt-4 text-lg font-bold text-gray-900 sm:text-xl">
-          Science of Chemistry
+          C__mis__y
         </h3>
 
         <p className="mt-2 hidden text-sm sm:block">
-          You can manage phone, email and chat conversations all from a single
-          mailbox.
+          Once you are sure of the word, proceed to stake the required amount
+          and solve the word in the 30 seconds period to be rewarded
         </p>
       </div>
 
       <span
-        className="rounded-full bg-green-100 px-3 py-1.5 text-xs font-medium text-green-600"
+        className="rounded-full bg-blue-100 px-3 py-1.5 text-xs font-medium text-blue-600"
       >
-        4.3
+        Solve
       </span>
     </a>
         </div>
