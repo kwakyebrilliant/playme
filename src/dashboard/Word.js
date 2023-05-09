5cd537764e2d1b0d27c6dbc673212b6dd3240268
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-05-09 13:24:41 +0000

    word updates

--- src/dashboard/Word.js
+++ src/dashboard/Word.js
@@ -32,44 +32,12 @@ function Word() {
 
         <div className='lg:pt-8 lg:px-24 p-4'>
 
-        <div className='w-full h-screen pt-20 px-20 bg-white flex flex-col justify-between'>
-        <div className='grid md:grid-cols-2 max-w-[1240px] m-auto'>
+        <div className='w-full pt-20 px-20 bg-white flex flex-col justify-between'>
+        <div className='grid md:grid-cols-2 max-w-[1240px]'>
             <div className='flex flex-col justify-center md:items-start w-full px-2 py-8'>
                 <h1 className='py-3 text-4xl md:text-6xl font-bold'>Climate <strong className="font-extrabold text-green-600 sm:block">Actions</strong></h1>
                 <p className='max-w-lg sm:leading-relaxed sm:text-xl'>Champion, solicit and undertake </p>
                 <p className='max-w-lg sm:leading-relaxed sm:text-xl'>actions to help safeguard our planet.</p>
-                <div className="flex flex-wrap gap-4 mt-8 text-center">
-                <a className="group relative inline-flex items-center overflow-hidden rounded bg-green-600 px-8 py-3 text-white focus:outline-none focus:ring active:bg-green-600" href="/summary">
-                <span className="absolute left-0 -translate-x-full transition-transform group-hover:translate-x-4">
-                    <svg
-                    className="h-5 w-5"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    >
-                    <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        stroke-width="2"
-                        d="M17 8l4 4m0 0l-4 4m4-4H3"
-                    />
-                    </svg>
-                </span>
-
-                <span className="text-sm font-medium transition-all group-hover:ml-4">
-                    Get Started
-                </span>
-                </a>
-
-                <a data-role="smoothscroll" className="group relative inline-block overflow-hidden border border-green-600 px-8 py-3 focus:outline-none focus:ring" href="/about">
-                <span className="absolute inset-x-0 top-0 h-[2px] bg-green-600 transition-all group-hover:h-full group-active:bg-green-600"></span>
-
-                <span className="relative text-sm font-medium text-green-600 transition-colors group-hover:text-white">
-                    Learn More
-                </span>
-                </a>
-                </div>
 
             </div>
             
