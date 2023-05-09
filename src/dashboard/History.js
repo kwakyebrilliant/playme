d6e262d2f128133acb27789f634723f56663b855
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-05-09 12:39:47 +0000

    history updates

--- src/dashboard/History.js
+++ src/dashboard/History.js
@@ -26,68 +26,68 @@ function History() {
         
       <div className='lg:pt-8 lg:px-24 p-4'>
 
-<div className='bg-white border flex justify-between lg:p-4 p-2'>
-  <div>
-  <FaMoneyCheck className=' text-blue-600 lg:w-[35px] lg:h-[35px]'/>
-  </div>
-  <div>
-  <p className='text-lg'>+ 0.02556988 ETH</p>
-  <p className='text-blue-400 text-sm'>+ $47.41 USD</p>
-  </div>
-  </div>
+            <div className='bg-white border flex justify-between lg:p-4 p-2'>
+            <div>
+            <FaMoneyCheck className=' text-blue-600 lg:w-[35px] lg:h-[35px]'/>
+            </div>
+            <div>
+            <p className='text-lg'>+ 0.02556988 ETH</p>
+            <p className='text-blue-400 text-sm'>+ $47.41 USD</p>
+            </div>
+            </div>
 
-  <div className='bg-white border flex justify-between lg:p-4 p-2'>
-  <div>
-  <FaMoneyCheck className=' text-blue-600 lg:w-[35px] lg:h-[35px]'/>
-  </div>
-  <div>
-  <p className='text-lg'>+ 0.02556988 ETH</p>
-  <p className='text-blue-400 text-sm'>+ $47.41 USD</p>
-  </div>
-  </div>
+            <div className='bg-white border flex justify-between lg:p-4 p-2'>
+            <div>
+            <FaMoneyCheck className=' text-blue-600 lg:w-[35px] lg:h-[35px]'/>
+            </div>
+            <div>
+            <p className='text-lg'>+ 0.02556988 ETH</p>
+            <p className='text-blue-400 text-sm'>+ $47.41 USD</p>
+            </div>
+            </div>
 
-  <div className='bg-white border flex justify-between lg:p-4 p-2'>
-  <div>
-  <FaMoneyCheck className=' text-blue-600 lg:w-[35px] lg:h-[35px]'/>
-  </div>
-  <div>
-  <p className='text-lg'>+ 0.02556988 ETH</p>
-  <p className='text-blue-400 text-sm'>+ $47.41 USD</p>
-  </div>
-  </div>
+            <div className='bg-white border flex justify-between lg:p-4 p-2'>
+            <div>
+            <FaMoneyCheck className=' text-blue-600 lg:w-[35px] lg:h-[35px]'/>
+            </div>
+            <div>
+            <p className='text-lg'>+ 0.02556988 ETH</p>
+            <p className='text-blue-400 text-sm'>+ $47.41 USD</p>
+            </div>
+            </div>
 
-  <div className='bg-white border flex justify-between lg:p-4 p-2'>
-  <div>
-  <FaMoneyCheck className=' text-blue-600 lg:w-[35px] lg:h-[35px]'/>
-  </div>
-  <div>
-  <p className='text-lg'>+ 0.02556988 ETH</p>
-  <p className='text-blue-400 text-sm'>+ $47.41 USD</p>
-  </div>
-  </div>
+            <div className='bg-white border flex justify-between lg:p-4 p-2'>
+            <div>
+            <FaMoneyCheck className=' text-blue-600 lg:w-[35px] lg:h-[35px]'/>
+            </div>
+            <div>
+            <p className='text-lg'>+ 0.02556988 ETH</p>
+            <p className='text-blue-400 text-sm'>+ $47.41 USD</p>
+            </div>
+            </div>
 
-  <div className='bg-white border flex justify-between lg:p-4 p-2'>
-  <div>
-  <FaMoneyCheck className=' text-blue-600 lg:w-[35px] lg:h-[35px]'/>
-  </div>
-  <div>
-  <p className='text-lg'>+ 0.02556988 ETH</p>
-  <p className='text-blue-400 text-sm'>+ $47.41 USD</p>
-  </div>
-  </div>
+            <div className='bg-white border flex justify-between lg:p-4 p-2'>
+            <div>
+            <FaMoneyCheck className=' text-blue-600 lg:w-[35px] lg:h-[35px]'/>
+            </div>
+            <div>
+            <p className='text-lg'>+ 0.02556988 ETH</p>
+            <p className='text-blue-400 text-sm'>+ $47.41 USD</p>
+            </div>
+            </div>
 
-  <div className='bg-white border flex justify-between lg:p-4 p-2'>
-  <div>
-  <FaMoneyCheck className=' text-blue-600 lg:w-[35px] lg:h-[35px]'/>
-  </div>
-  <div>
-  <p className='text-lg'>+ 0.02556988 ETH</p>
-  <p className='text-blue-400 text-sm'>+ $47.41 USD</p>
-  </div>
-  </div>
-  
+            <div className='bg-white border flex justify-between lg:p-4 p-2'>
+            <div>
+            <FaMoneyCheck className=' text-blue-600 lg:w-[35px] lg:h-[35px]'/>
+            </div>
+            <div>
+            <p className='text-lg'>+ 0.02556988 ETH</p>
+            <p className='text-blue-400 text-sm'>+ $47.41 USD</p>
+            </div>
+            </div>
+            
 
-</div>
+            </div>
 
     </div>
   )
