f69771fed2ea5c361a85b74e6ce0e19f53c73246
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-05-09 12:38:02 +0000

    history updates

--- src/dashboard/History.js
+++ src/dashboard/History.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import DashboardNav from './DashboardNav'
 
+import { FaMoneyCheck } from 'react-icons/fa'
+
 function History() {
   return (
     <div className='bg-gray-100'>
@@ -22,7 +24,70 @@ function History() {
         </div>
       </div>
         
-        History
+      <div className='lg:pt-8 lg:px-24 p-4'>
+
+<div className='bg-white border flex justify-between lg:p-4 p-2'>
+  <div>
+  <FaMoneyCheck className=' text-blue-600 lg:w-[35px] lg:h-[35px]'/>
+  </div>
+  <div>
+  <p className='text-lg'>+ 0.02556988 ETH</p>
+  <p className='text-blue-400 text-sm'>+ $47.41 USD</p>
+  </div>
+  </div>
+
+  <div className='bg-white border flex justify-between lg:p-4 p-2'>
+  <div>
+  <FaMoneyCheck className=' text-blue-600 lg:w-[35px] lg:h-[35px]'/>
+  </div>
+  <div>
+  <p className='text-lg'>+ 0.02556988 ETH</p>
+  <p className='text-blue-400 text-sm'>+ $47.41 USD</p>
+  </div>
+  </div>
+
+  <div className='bg-white border flex justify-between lg:p-4 p-2'>
+  <div>
+  <FaMoneyCheck className=' text-blue-600 lg:w-[35px] lg:h-[35px]'/>
+  </div>
+  <div>
+  <p className='text-lg'>+ 0.02556988 ETH</p>
+  <p className='text-blue-400 text-sm'>+ $47.41 USD</p>
+  </div>
+  </div>
+
+  <div className='bg-white border flex justify-between lg:p-4 p-2'>
+  <div>
+  <FaMoneyCheck className=' text-blue-600 lg:w-[35px] lg:h-[35px]'/>
+  </div>
+  <div>
+  <p className='text-lg'>+ 0.02556988 ETH</p>
+  <p className='text-blue-400 text-sm'>+ $47.41 USD</p>
+  </div>
+  </div>
+
+  <div className='bg-white border flex justify-between lg:p-4 p-2'>
+  <div>
+  <FaMoneyCheck className=' text-blue-600 lg:w-[35px] lg:h-[35px]'/>
+  </div>
+  <div>
+  <p className='text-lg'>+ 0.02556988 ETH</p>
+  <p className='text-blue-400 text-sm'>+ $47.41 USD</p>
+  </div>
+  </div>
+
+  <div className='bg-white border flex justify-between lg:p-4 p-2'>
+  <div>
+  <FaMoneyCheck className=' text-blue-600 lg:w-[35px] lg:h-[35px]'/>
+  </div>
+  <div>
+  <p className='text-lg'>+ 0.02556988 ETH</p>
+  <p className='text-blue-400 text-sm'>+ $47.41 USD</p>
+  </div>
+  </div>
+  
+
+</div>
 
     </div>
   )
