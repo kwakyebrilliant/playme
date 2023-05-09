8abab10cc2457d978d35fd8f6f4cb2b80f4bad07
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-05-09 17:10:50 +0000

    words updates

--- src/dashboard/Word.js
+++ src/dashboard/Word.js
@@ -79,6 +79,153 @@ function Word() {
         </div> 
         
         </div>
+
+        <div className="relative">
+        <div className="flex items-center lg:pt-8 lg:px-24 p-4">
+         <p className='text-2xl font-bold'>Others</p>  <div className="w-full border-b border-gray-300"></div>
+        </div>
+        </div>
+
+
+        <div className='lg:pt-8 lg:px-24 p-4'>
+
+        <div className='grid md:grid-cols-2 gap-x-10 lg:p-4 p-2'>
+        
+        <div>
+        <a style={{ backgroundImage: `url(${trophy})` }} className="relative block overflow-hidden rounded-xl bg-cover bg-center bg-no-repeat">
+          <div className="absolute inset-0 bg-black/25"></div>
+
+          <div className="relative flex items-start justify-between p-4 sm:p-6 lg:p-8">
+            <div className="sm:pt-18 pt-12 text-white lg:pt-24">
+              <h3 className="text-xl font-bold sm:text-2xl">$47.41 USD</h3>
+
+              <p className="text-sm">Reward</p>
+            </div>
+          </div>
+        </a>
+        </div>
+
+        <div>
+            <a className="relative flex items-start justify-between rounded-xl bg-white border border-gray-100 p-4 shadow-xl sm:p-6 lg:p-8">
+      <div className="pt-4 text-gray-500">
+      <FaRegFileWord className=' text-gray-500 lg:w-8 lg:h-8 sm:h-10 sm:w-10'/>
+
+        <h3 className="mt-4 text-lg font-bold text-gray-900 sm:text-xl">
+          C__mis__y
+        </h3>
+
+        <p className="mt-2 hidden text-sm sm:block">
+          Once you are sure of the word, proceed to stake the required amount
+          and solve the word in the 30 seconds period to be rewarded
+        </p>
+      </div>
+
+      <span
+        className="rounded-full bg-blue-100 px-3 py-1.5 text-xs font-medium text-blue-600"
+      >
+        Solve
+      </span>
+    </a>
+        </div>
+       
+
+        </div> 
+        
+        </div>
+
+
+        <div className='lg:pt-8 lg:px-24 p-4'>
+
+        <div className='grid md:grid-cols-2 gap-x-10 lg:p-4 p-2'>
+        
+        <div>
+        <a style={{ backgroundImage: `url(${trophy})` }} className="relative block overflow-hidden rounded-xl bg-cover bg-center bg-no-repeat">
+          <div className="absolute inset-0 bg-black/25"></div>
+
+          <div className="relative flex items-start justify-between p-4 sm:p-6 lg:p-8">
+            <div className="sm:pt-18 pt-12 text-white lg:pt-24">
+              <h3 className="text-xl font-bold sm:text-2xl">$47.41 USD</h3>
+
+              <p className="text-sm">Reward</p>
+            </div>
+          </div>
+        </a>
+        </div>
+
+        <div>
+            <a className="relative flex items-start justify-between rounded-xl bg-white border border-gray-100 p-4 shadow-xl sm:p-6 lg:p-8">
+      <div className="pt-4 text-gray-500">
+      <FaRegFileWord className=' text-gray-500 lg:w-8 lg:h-8 sm:h-10 sm:w-10'/>
+
+        <h3 className="mt-4 text-lg font-bold text-gray-900 sm:text-xl">
+          C__mis__y
+        </h3>
+
+        <p className="mt-2 hidden text-sm sm:block">
+          Once you are sure of the word, proceed to stake the required amount
+          and solve the word in the 30 seconds period to be rewarded
+        </p>
+      </div>
+
+      <span
+        className="rounded-full bg-blue-100 px-3 py-1.5 text-xs font-medium text-blue-600"
+      >
+        Solve
+      </span>
+    </a>
+        </div>
+       
+
+        </div> 
+        
+        </div>
+
+
+        <div className='lg:pt-8 lg:px-24 p-4'>
+
+        <div className='grid md:grid-cols-2 gap-x-10 lg:p-4 p-2'>
+        
+        <div>
+        <a style={{ backgroundImage: `url(${trophy})` }} className="relative block overflow-hidden rounded-xl bg-cover bg-center bg-no-repeat">
+          <div className="absolute inset-0 bg-black/25"></div>
+
+          <div className="relative flex items-start justify-between p-4 sm:p-6 lg:p-8">
+            <div className="sm:pt-18 pt-12 text-white lg:pt-24">
+              <h3 className="text-xl font-bold sm:text-2xl">$47.41 USD</h3>
+
+              <p className="text-sm">Reward</p>
+            </div>
+          </div>
+        </a>
+        </div>
+
+        <div>
+            <a className="relative flex items-start justify-between rounded-xl bg-white border border-gray-100 p-4 shadow-xl sm:p-6 lg:p-8">
+      <div className="pt-4 text-gray-500">
+      <FaRegFileWord className=' text-gray-500 lg:w-8 lg:h-8 sm:h-10 sm:w-10'/>
+
+        <h3 className="mt-4 text-lg font-bold text-gray-900 sm:text-xl">
+          C__mis__y
+        </h3>
+
+        <p className="mt-2 hidden text-sm sm:block">
+          Once you are sure of the word, proceed to stake the required amount
+          and solve the word in the 30 seconds period to be rewarded
+        </p>
+      </div>
+
+      <span
+        className="rounded-full bg-blue-100 px-3 py-1.5 text-xs font-medium text-blue-600"
+      >
+        Solve
+      </span>
+    </a>
+        </div>
+       
+
+        </div> 
+        
+        </div>
       
 
     </div>
