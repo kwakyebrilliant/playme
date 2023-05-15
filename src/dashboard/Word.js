23aa57ca0c5a890b58394e56965d7bd9a81ba623
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-05-15 21:53:46 +0000

    word updates

--- src/dashboard/Word.js
+++ src/dashboard/Word.js
@@ -88,7 +88,9 @@ function Word() {
         {isOpen && (
         <div className="fixed inset-0 flex items-center justify-center z-50">
           <div className="bg-white w-1/2 rounded-lg shadow-lg p-6">
-            <h2 className="text-lg font-semibold mb-4">Popup Content</h2>
+            <h2 className="text-lg font-semibold mb-4">
+            C__mis__y
+            </h2>
             <p>This is the content of the popup.</p>
             <button
               className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4"
