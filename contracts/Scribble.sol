9972aea56991f3c707a1bf7f552e2b045816e295
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-05-25 12:19:02 +0000

    scribble updates

--- contracts/Scribble.sol
+++ contracts/Scribble.sol
@@ -1,2 +1,10 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.4;
+
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
+contract WordScribble {
+
+    
+
+}
