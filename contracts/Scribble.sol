8607060d719b72b945ca90a44ce1f1c71a35a7ef
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2023-05-28 11:00:21 +0000

    scribble updates

--- contracts/Scribble.sol
+++ contracts/Scribble.sol
@@ -90,6 +90,17 @@ contract WordScribble {
     }
 
 
+    
+    function getLatestPrice() public view returns (int) {
+        (
+            uint80 roundID, 
+            int price,
+            uint startedAt,
+            uint timeStamp,
+            uint80 answeredInRound
+        ) = priceFeed.latestRoundData();
+        return price;
+    }
 
 
 }
